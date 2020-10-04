07a85f2f0624bd6fc53ef10f117cdeccc856d6e8
Łukasz Stachowiak <easy4u3@gmail.com>
2020-10-04 20:53:29 +0200

    Working version of App

--- app/src/main/java/com/example/corona_project/MainActivity.kt
+++ app/src/main/java/com/example/corona_project/MainActivity.kt
@@ -2,20 +2,95 @@ package com.example.corona_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.github.kittinunf.fuel.httpGet
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
-import java.net.URL
-import kotlin.math.log
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val result = URL("http://date.jsontest.com/").readText()
+        "https://coronavirus-19-api.herokuapp.com/countries"
+            .httpGet().responseObject(Post.Deserializer()) { _, _, result ->
+                val coronaData = result.component1()
+                if (coronaData != null) {
+                    val countries = resources.getStringArray(R.array.Countries)
+                    val spinner = findViewById<Spinner>(R.id.spinner1)
 
-        Log.i("TAG", result);
+                    if (spinner != null) {
+                        val adapter = ArrayAdapter(
+                            this,
+                            android.R.layout.simple_spinner_item, countries
+                        )
+                        spinner.adapter = adapter
 
+                        spinner.onItemSelectedListener = object :
+                            AdapterView.OnItemSelectedListener {
+                            override fun onNothingSelected(p0: AdapterView<*>?) {
+                                for(i in coronaData.indices){
+                                    if(coronaData[i].country == "Poland"){
+                                        todayCases.text = "Today cases: " + coronaData[i].todayCases
+                                        todayDeaths.text = "Today deaths: " + coronaData[i].todayDeaths
+                                        cases.text = "Total cases: " + coronaData[i].cases
+                                        deaths.text = "Total deaths: " + coronaData[i].deaths
+                                        recovered.text = "Recovered: " + coronaData[i].recovered
+                                        break
+                                    }
+                                }
+                            }
+
+                            override fun onItemSelected(
+                                parent: AdapterView<*>,
+                                view: View, position: Int, id: Long
+                            ) {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    getString(R.string.selected_item) + " " +
+                                            "" + countries[position], Toast.LENGTH_SHORT
+                                ).show()
+
+                                //////
+                                for(i in coronaData.indices){
+                                    if(coronaData[i].country == countries[position]){
+                                        todayCases.text = "Today cases: " + coronaData[i].todayCases
+                                        todayDeaths.text = "Today deaths: " + coronaData[i].todayDeaths
+                                        cases.text = "Total cases: " + coronaData[i].cases
+                                        deaths.text = "Total deaths: " + coronaData[i].deaths
+                                        recovered.text = "Recovered: " + coronaData[i].recovered
+                                        break
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+    }
+
+}
+
+data class Post(var country:String,
+                var cases:String,
+                var todayCases:String,
+                var deaths:String,
+                var todayDeaths:String,
+                var recovered:String,
+                var active:String,
+                var critical:String,
+                var casesPerOneMillion:String,
+                var deathsPerOneMillion:String,
+                var totalTestes:String,
+                var testsPerOneMillion:String){
+
+    class Deserializer : ResponseDeserializable<Array<Post>> {
+        override fun deserialize(content: String): Array<Post>
+                = Gson().fromJson(content, Array<Post>::class.java)
     }
 }
